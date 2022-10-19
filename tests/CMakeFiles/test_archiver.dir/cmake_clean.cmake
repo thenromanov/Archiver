0cc5d427e15e69b59a508ecbeb5ab8ae4bcf192d
Nikita Romanov <thenromanov@gmail.com>
2022-10-19 21:00:14 +0300

    Initial commit

---
+++ tests/CMakeFiles/test_archiver.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_archiver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/test_archiver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
