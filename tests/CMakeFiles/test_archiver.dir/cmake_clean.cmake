6db7e501e7e0297c25c79900bcaf17f80f3fbc8c
Nikita Romanov <thenromanov@gmail.com>
2022-10-19 23:40:00 +0300

    Removed build files

--- tests/CMakeFiles/test_archiver.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_archiver"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_archiver.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
